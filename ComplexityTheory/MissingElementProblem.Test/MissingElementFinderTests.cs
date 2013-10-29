fe9492a230cb72d17f553bda2dead2961657d154
ghostsquad <ghost.squadron@gmail.com>
2013-10-28 18:45:52 -0700

    add int.max/min tests. class cleanup.

--- ComplexityTheory/MissingElementProblem.Test/MissingElementFinderTests.cs
+++ ComplexityTheory/MissingElementProblem.Test/MissingElementFinderTests.cs
@@ -1,24 +1,14 @@
 ﻿namespace MissingElementProblem.Test
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
 
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using Ploeh.AutoFixture;
 
     [TestClass]
     [ExcludeFromCodeCoverage]
     public class MissingElementFinderTests
     {
-        [TestMethod]
-        public void GivenConsectiveNumbers()
-        {
-            const int ExpectedResult = 2;
-            var array1 = new int?[] { 1, 2, 3 };
-            var array2 = new int?[] { 1, null, 3 };            
-
-            this.AssertResults(array1, array2, ExpectedResult);
-        }
+        #region Public Methods and Operators
 
         [TestMethod]
         public void FirstArrayWithMissingValue()
@@ -27,27 +17,17 @@
             var array1 = new int?[] { 1, null, 3 };
             var array2 = new int?[] { 1, 5, 3 };
 
-            this.AssertResults(array1, array2, ExpectedResult);
+            AssertResults(array1, array2, ExpectedResult);
         }
 
         [TestMethod]
-        public void GivenConsectiveNumbersWithZero()
+        public void GivenConsectiveNumbers()
         {
-            var array1 = new int?[] { 0, 1, 2, 3 };
-            var array2 = new int?[] { 0, 1, null, 3 };
             const int ExpectedResult = 2;
+            var array1 = new int?[] { 1, 2, 3 };
+            var array2 = new int?[] { 1, null, 3 };
 
-            this.AssertResults(array1, array2, ExpectedResult);
-        }
-
-        [TestMethod]
-        public void GivenConsectiveNumbersWithZeroOutOfOrder()
-        {
-            var array1 = new int?[] { 3, 1, 2, 0 };
-            var array2 = new int?[] { 0, 1, 3, null };
-            const int ExpectedResult = 2;
-
-            this.AssertResults(array1, array2, ExpectedResult);
+            AssertResults(array1, array2, ExpectedResult);
         }
 
         [TestMethod]
@@ -57,17 +37,47 @@
             var array2 = new int?[] { null, 1, 2, 3 };
             const int ExpectedResult = 0;
 
-            this.AssertResults(array1, array2, ExpectedResult);
+            AssertResults(array1, array2, ExpectedResult);
         }
 
         [TestMethod]
-        public void NonConsectiveNumbersOutOfOrder()
+        public void GivenConsectiveNumbersWithZero()
         {
-            var array1 = new int?[] { 10, 20, 60, 32 };
-            var array2 = new int?[] { null, 60, 20, 32 };
-            const int ExpectedResult = 10;
+            var array1 = new int?[] { 0, 1, 2, 3 };
+            var array2 = new int?[] { 0, 1, null, 3 };
+            const int ExpectedResult = 2;
 
-            this.AssertResults(array1, array2, ExpectedResult);
+            AssertResults(array1, array2, ExpectedResult);
+        }
+
+        [TestMethod]
+        public void GivenConsectiveNumbersWithZeroOutOfOrder()
+        {
+            var array1 = new int?[] { 3, 1, 2, 0 };
+            var array2 = new int?[] { 0, 1, 3, null };
+            const int ExpectedResult = 2;
+
+            AssertResults(array1, array2, ExpectedResult);
+        }
+
+        [TestMethod]
+        public void IntMax()
+        {
+            var array1 = new int?[] { 1, int.MaxValue, 60, 32 };
+            var array2 = new int?[] { 1, 60, null, 32 };
+            const int ExpectedResult = int.MaxValue;
+
+            AssertResults(array1, array2, ExpectedResult);
+        }
+
+        [TestMethod]
+        public void IntMin()
+        {
+            var array1 = new int?[] { 1, int.MinValue, 60, 32 };
+            var array2 = new int?[] { 1, 60, null, 32 };
+            const int ExpectedResult = int.MinValue;
+
+            AssertResults(array1, array2, ExpectedResult);
         }
 
         [TestMethod]
@@ -77,14 +87,30 @@
             var array2 = new int?[] { null, 60, 20, 32 };
             const int ExpectedResult = -10;
 
-            this.AssertResults(array1, array2, ExpectedResult);
+            AssertResults(array1, array2, ExpectedResult);
         }
 
-        private void AssertResults(int?[] array1, int?[] array2, int expectedResult)
+        [TestMethod]
+        public void NonConsectiveNumbersOutOfOrder()
+        {
+            var array1 = new int?[] { 10, 20, 60, 32 };
+            var array2 = new int?[] { null, 60, 20, 32 };
+            const int ExpectedResult = 10;
+
+            AssertResults(array1, array2, ExpectedResult);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static void AssertResults(int?[] array1, int?[] array2, int expectedResult)
         {
             var actualResult = MissingElementFinder.GetMissingElement(array1, array2);
 
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        #endregion
     }
 }
