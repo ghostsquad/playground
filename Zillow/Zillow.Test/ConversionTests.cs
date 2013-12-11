2c3d41709453c182847ec1ef4e8335c03e13cb45
Wes <ghost.squadron@gmail.com>
2013-12-10 21:54:28 -0800

    string to long varieties + tests and 100% code coverage

---
+++ Zillow/Zillow.Test/ConversionTests.cs
@@ -0,0 +1,203 @@
+﻿namespace Zillow.Test
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using Moq;
+
+    using Ploeh.AutoFixture;
+
+    [TestClass]
+    public class ConversionTests
+    {
+        private const long BasicExpected = 123;
+
+        private const string BasicValue = "123";
+
+        [TestMethod]
+        public void GivenZillowTestWhenIndividualIntExpectLong()
+        {
+            // act
+            var actual = Conversions.StringToLong(BasicValue);
+
+            // assert
+            Assert.AreEqual<long>(BasicExpected, actual);
+        }
+
+        [TestMethod]
+        public void GivenZillowTestWhenRoundedExpectLong()
+        {
+            // act
+            var actual = Conversions.StringToLong(BasicValue, Conversions.ConversionMethods.Rounded);
+
+            // assert
+            Assert.AreEqual<long>(BasicExpected, actual);
+        }
+
+        [TestMethod]
+        public void GivenZillowTestWhenTryParseExpectLong()
+        {
+            // act
+            var actual = Conversions.StringToLong(BasicValue, Conversions.ConversionMethods.TryParse);
+
+            // assert
+            Assert.AreEqual<long>(BasicExpected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void GivenNotImplementedMethodExpectException()
+        {
+            // act
+            Conversions.StringToLong(BasicValue, Conversions.ConversionMethods.NotImplementedMethod);
+        }
+
+        [TestMethod]
+        public void GivenLongMaxValue()
+        {
+            // arrange
+            var expected = long.MaxValue;
+            var value = expected.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenNegativeInteger()
+        {
+            // arrange
+            var fixture = new Fixture();
+            long expected = 0 - fixture.Create<uint>();
+            var value = expected.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenLongMinValue()
+        {
+            // arrange
+            var expected = long.MinValue;
+            var value = expected.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenZero()
+        {
+            // arrange
+            long expected = 0;
+            var value = expected.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GivenNumberToBigExpectException()
+        {
+            // arrange
+            var longMaxPlusOne = "9223372036854775808";
+
+            // act
+            Conversions.StringToLong(longMaxPlusOne);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void GivenNumberTooSmallExpectException()
+        {
+            // arrange
+            var longMinMinusOne = "-9223372036854775809";
+
+            // act
+            Conversions.StringToLong(longMinMinusOne);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GivenInCorrectFormatWhenIndividualIntegersExpectException()
+        {
+            // arrange
+            var fixture = new Fixture();
+            
+            // default strings by autofixture will be in the format of guids
+            var value = fixture.Create<string>();
+
+            // act
+            Conversions.StringToLong(value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GivenIncorrectDoubleDecimalFormatWhenIndividualIntegersExpectException()
+        {
+            // arrange
+            var value = "123.456.789";            
+
+            // act
+            Conversions.StringToLong(value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GivenIncorrectDoubleNegativeFormatWhenIndividualIntegersExpectException()
+        {
+            // arrange
+            var value = "--1";
+
+            // act
+            Conversions.StringToLong(value);
+        }
+
+        [TestMethod]
+        public void GivenDoubleWhenRoundedExpectLong()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var expectedAsDouble = fixture.Create<double>();
+            var expected = (long)expectedAsDouble;
+            var value = expectedAsDouble.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value, Conversions.ConversionMethods.Rounded);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+
+        [TestMethod]
+        public void GivenNegativeDoubleWhenRoundedExpectLong()
+        {
+            // arrange
+            var fixture = new Fixture();
+            var expectedAsDouble = 0 - fixture.Create<double>();
+            var expected = (long)expectedAsDouble;
+            var value = expectedAsDouble.ToString();
+
+            // act
+            var actual = Conversions.StringToLong(value, Conversions.ConversionMethods.Rounded);
+
+            // assert
+            Assert.AreEqual<long>(expected, actual);
+        }
+    }
+}
---
+++ Zillow/Zillow/Conversions.cs
@@ -0,0 +1,271 @@
+﻿namespace Zillow
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    ///     The conversions.
+    /// </summary>
+    public static class Conversions
+    {
+        #region Static Fields
+
+        /// <summary>
+        ///     The regex decimal.
+        /// </summary>
+        private static readonly Regex RegexDecimal = new Regex(@"^-?\d+(\.\d+)?$");
+
+        /// <summary>
+        ///     The regex digits.
+        /// </summary>
+        private static readonly Regex RegexDigits = new Regex(@"^-?\d+$");
+
+        #endregion
+
+        #region Enums
+
+        /// <summary>
+        ///     The conversion methods.
+        /// </summary>
+        public enum ConversionMethods
+        {
+            /// <summary>
+            ///     The individual integers conversion method.
+            /// </summary>
+            IndividualIntegers, 
+
+            /// <summary>
+            ///     The try parse method.
+            /// </summary>
+            TryParse, 
+
+            /// <summary>
+            ///     The rounded method.
+            /// </summary>
+            Rounded, 
+
+            /// <summary>
+            ///     The not implemented method. I'm adding this as a way to demonstrate the extensibility of this class
+            ///     that all enum values require an entry in the switch statement.
+            ///     Alternatives to this is to implement the Strategy pattern.
+            /// </summary>
+            NotImplementedMethod
+        }
+
+        /// <summary>
+        /// The math operations.
+        /// </summary>
+        private enum MathOperations
+        {
+            /// <summary>
+            /// The addition.
+            /// </summary>
+            Addition, 
+
+            /// <summary>
+            /// The subtraction.
+            /// </summary>
+            Subtraction
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        /// <summary>
+        /// The string to long.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        public static long StringToLong(string value)
+        {
+            return StringToLong(value, ConversionMethods.IndividualIntegers);
+        }
+
+        /// <summary>
+        /// The string to long.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <param name="conversionMethod">
+        /// The conversion type.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        public static long StringToLong(string value, ConversionMethods conversionMethod)
+        {
+            switch (conversionMethod)
+            {
+                case ConversionMethods.IndividualIntegers:
+                    {
+                        return IndividualIntegers(value);
+                    }
+
+                case ConversionMethods.Rounded:
+                    {
+                        return Rounded(value);
+                    }
+
+                case ConversionMethods.TryParse:
+                    {
+                        return TryParse(value);
+                    }
+
+                default:
+                    {
+                        throw new NotImplementedException(
+                            string.Format("{0} Conversion Method is Not Supported.", conversionMethod));
+                    }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Walks each character of a string, converting to a integer, and adding it's representation together
+        ///     finally returning a long.
+        ///     Example:
+        ///     "123"
+        ///     becomes
+        ///     100 + 20 + 3
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// When the value contains characters that aren't digits (or a starting minus sign).
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// When the value is larger or smaller than what can be contained by a long value type.
+        /// </exception>
+        private static long IndividualIntegers(string value)
+        {
+            if (!RegexDigits.IsMatch(value))
+            {
+                throw new ArgumentException("Value must contain only digits (minus sign optional)");
+            }
+
+            bool isNegative = value[0] == '-';            
+
+            MathOperations operation = isNegative ? MathOperations.Subtraction : MathOperations.Addition;
+            int startPosition = isNegative ? 1 : 0;
+
+            long newLong = 0;
+
+            for (int i = startPosition; i < value.Length; i++)
+            {
+                var digit = int.Parse(value[i].ToString());
+                try
+                {
+                    checked
+                    {
+                        var exponent = (value.Length - 1) - i;
+                        long operand = digit * (long)Math.Pow(10, exponent);
+                        newLong = performOperation(newLong, operand, operation);
+                    }
+                }
+                catch (OverflowException)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        "value", 
+                        value, 
+                        "Value is larger or smaller than what can be stored as a long.");
+                }
+            }
+
+            return newLong;
+        }
+
+        /// <summary>
+        /// Given a string the is in the format of a positive or negative integer or decimal number,
+        ///     truncates the decimal places and returns the long value type representation.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// When the value does not represent an equivalent format to a positive or negative integer or decimal number.
+        /// </exception>
+        private static long Rounded(string value)
+        {
+            if (!RegexDecimal.IsMatch(value))
+            {
+                throw new ArgumentException("Value is not in a format that can be rounded to a long.");
+            }
+
+            return IndividualIntegers(value.Split(new[] { '.' })[0]);
+        }
+
+        /// <summary>
+        /// Uses long.TryParse to convert a string to a long.
+        /// </summary>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// If the string value cannot be parsed to a long value type.
+        /// </exception>
+        private static long TryParse(string value)
+        {
+            long newLong;
+            bool parseResult = long.TryParse(value, out newLong);
+
+            if (!parseResult)
+            {
+                throw new ArgumentException("Value cannot be parsed to a value type long.");
+            }
+
+            return newLong;
+        }
+
+        /// <summary>
+        /// The perform operation.
+        /// </summary>
+        /// <param name="num1">
+        /// The num 1.
+        /// </param>
+        /// <param name="num2">
+        /// The num 2.
+        /// </param>
+        /// <param name="operation">
+        /// The operation.
+        /// </param>
+        /// <returns>
+        /// The <see cref="long"/>.
+        /// </returns>
+        private static long performOperation(long num1, long num2, MathOperations operation)
+        {
+            if (operation == MathOperations.Addition)
+            {
+                checked
+                {
+                    return num1 + num2;
+                }
+            }
+
+            checked
+            {
+                return num1 - num2;
+            }
+        }
+
+        #endregion
+    }
+}
